ae2f02111ba722099246ea8afa980a4dd66e515b
melwin <114172287+itzTedx@users.noreply.github.com>
2024-07-26 18:06:35 +0400

    Suspense Boundary

--- src/app/(admin)/auth/new-verification/page.tsx
+++ src/app/(admin)/auth/new-verification/page.tsx
@@ -1,5 +1,10 @@
 import EmailVerificationForm from '@/components/auth/email-verification-form'
+import { Suspense } from 'react'
 
 export default function EmailVerification() {
-  return <EmailVerificationForm />
+  return (
+    <Suspense>
+      <EmailVerificationForm />
+    </Suspense>
+  )
 }
