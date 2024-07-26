e07e3ba42dd0cc2571a5a839e9aef2d44bc6229d
melwin <114172287+itzTedx@users.noreply.github.com>
2024-07-26 18:04:11 +0400

    Suspense Bound

--- src/app/(admin)/auth/new-password/page.tsx
+++ src/app/(admin)/auth/new-password/page.tsx
@@ -1,5 +1,10 @@
 import NewPasswordForm from '@/components/auth/new-password-form'
+import { Suspense } from 'react'
 
 export default function NewPass() {
-  return <NewPasswordForm />
+  return (
+    <Suspense>
+      <NewPasswordForm />
+    </Suspense>
+  )
 }
