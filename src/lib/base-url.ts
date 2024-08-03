fdd25f82e708247e5c65a6e310956720b9ff1878
itzTedx <zomboi.offi@gmail.com>
2024-08-03 08:27:28 +0000

    Base URL fix

--- src/lib/base-url.ts
+++ src/lib/base-url.ts
@@ -2,5 +2,6 @@ export default function getBaseURL() {
   if (typeof window !== 'undefined') return ''
 
   if (process.env.VERCEL_URL) return `https://${process.env.DOMAIN_URL}`
-  return 'http://localhost:3000'
+  if (process.env.NODE_ENV === 'development') return 'http://localhost:3000'
+  return `https://${process.env.NEXT_PUBLIC_SITE_URL}`
 }
